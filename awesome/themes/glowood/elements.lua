8499e98da149992c8eca78fd1ef9c7f2b448a7e0
Jack <htnjack@proton.me>
2023-12-08 11:35:53 +0100

    Gippity glowood theme

---
+++ awesome/themes/glowood/elements.lua
@@ -0,0 +1,59 @@
+local colors                  = require("themes.pinkmare.colors")
+
+local theme_assets         = require("beautiful.theme_assets")
+local xresources           = require("beautiful.xresources")
+local dpi                  = xresources.apply_dpi
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+theme.font                 = "Iosevka NF 8"  -- "Tamsyn 10" -- "Sans 8"
+theme.taglist_font         = "Inconsolata Medium 8"
+
+theme.bg_normal            = colors.bg0
+theme.bg_focus             = colors.bg2
+theme.bg_urgent            = colors.bg3
+theme.bg_minimize          = colors.bg0
+theme.bg_systray           = colors.bg0
+
+theme.fg_normal            = colors.fg
+theme.fg_focus             = colors.red
+theme.fg_urgent            = colors.fg
+theme.fg_minimize          = colors.fg
+
+theme.useless_gap          = dpi(0)
+theme.border_width         = dpi(1)
+
+theme.border_normal        = colors.bg3
+theme.border_focus         = colors.red
+theme.border_marked        = colors.bg3
+
+theme.taglist_bg_urgent    = colors.bg3
+theme.taglist_fg_urgent    = colors.fg
+theme.taglist_bg_occupied  = colors.bg1
+theme.taglist_fg_occupied  = colors.fg
+theme.taglist_bg_empty     = colors.bg0
+theme.taglist_fg_empty     = colors.fg
+theme.taglist_bg_focus     = colors.bg2
+theme.taglist_fg_focus     = colors.red
+
+theme.tasklist_bg_normal   = colors.bg0
+theme.tasklist_fg_normal   = colors.fg
+theme.tasklist_bg_focus    = colors.bg2
+theme.tasklist_fg_focus    = colors.red
+
+theme.titlebar_bg_normal   = colors.bg0
+theme.titlebar_fg_normal   = colors.fg
+theme.titlebar_bg_focus    = colors.bg2
+theme.titlebar_fg_focus    = colors.red
+
+-- theme.tooltip_font         = "Iosevka NF 8"
+-- theme.tooltip_opacity      = 0.9
+-- theme.tooltip_fg_color     = gmc.white
+-- theme.tooltip_bg_color     = gmc.red
+-- theme.tooltip_border_width = dpi(2)
+-- theme.tooltip_border_color = gmc.bg2
+
+local taglist_square_size  = dpi(4)
+theme.taglist_squares_sel  = theme_assets.taglist_squares_sel(
+    taglist_square_size, colors.red
+)
