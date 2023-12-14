29ddee8fb40ddc39985e4033b8e0ba6400c006c9
Jack <htnjack@proton.me>
2023-12-15 00:21:22 +0100

    starting cleanup/refactor

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -1,39 +1,21 @@
 pcall(require, "luarocks.loader")
 
--- Standard awesome library
-local gears = require("gears")
 local awful = require("awful")
+
 require('awful.autofocus')
 
--- Theme handling library
-local beautiful = require("beautiful")
-
--- Miscellanous awesome library
-local menubar = require("menubar")
-
-RC = {} -- global namespace, on top before require any modules
+RC = {}
 RC.vars = require("main.user-variables")
-modkey = RC.vars.modkey
 
--- Error handling
+require("main.signals")
 require("main.error-handling")
+require("deco.statusbar")
 
--- Themes
-require("main.theme")
+RC.theme = require("main.theme")
+RC.layouts = require("main.layouts")
+RC.tags    = require("main.tags")
+RC.rules   = require("main.rules")
 
--- -- --
-
--- Calling All Module Libraries
-
--- Custom Local Library
-local main = {
-    layouts = require("main.layouts"),
-    tags    = require("main.tags"),
-    -- menu    = require("main.menu"),
-    rules   = require("main.rules"),
-}
-
--- Custom Local Library: Keys and Mouse Binding
 local binding = {
     globalbuttons = require("binding.globalbuttons"),
     clientbuttons = require("binding.clientbuttons"),
@@ -42,41 +24,17 @@ local binding = {
     clientkeys    = require("binding.clientkeys")
 }
 
--- Layouts
-RC.layouts = main.layouts()
-
--- Tags
-RC.tags = main.tags()
-
--- Menu
--- RC.mainmenu = awful.menu({ items = main.menu() }) -- in globalkeys
--- RC.launcher = awful.widget.launcher(
---     { image = beautiful.awesome_icon, menu = RC.mainmenu }
--- )
-menubar.utils.terminal = RC.vars.terminal
-
--- Mouse and Key bindings
 RC.globalkeys = binding.globalkeys()
 RC.globalkeys = binding.bindtotags(RC.globalkeys)
-
--- Set root
 root.buttons(binding.globalbuttons())
 root.keys(RC.globalkeys)
-
--- Keyboard map indicator and switcher
-mykeyboardlayout = awful.widget.keyboardlayout()
-
--- Statusbar: Wibar
-require("deco.statusbar")
-
--- Rules
-awful.rules.rules = main.rules(
+awful.rules.rules = RC.rules(
     binding.clientkeys(),
     binding.clientbuttons()
 )
 
--- Signals
-require("main.signals")
+RC.layouts = RC.layouts()
+RC.tags = RC.tags()
 
--- Run utilities
+
 awful.spawn.with_shell("~/.config/awesome/autorun.sh")
