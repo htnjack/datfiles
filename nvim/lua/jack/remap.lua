2cc0908f88326f82ab04f4553902c3da0fe763da
Jack <htnjack@proton.me>
2023-12-15 00:20:56 +0100

    leader cc for cclose

--- nvim/lua/jack/remap.lua
+++ nvim/lua/jack/remap.lua
@@ -3,3 +3,5 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>w", vim.cmd.w)
 vim.keymap.set("n", "<leader>q", vim.cmd.quit)
+vim.keymap.set('n', '<leader>cc', ':cclose<CR>', { noremap = true, silent = true })
+
